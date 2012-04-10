71b94e248583cbe5bb1be6f111968e4aa7e1c6a1
Mark Hoad <markdhoad@gmail.com>
2012-04-11 00:05:13 +1000

    Finish chapter 4

---
+++ exercises.rb
@@ -0,0 +1,26 @@
+#Exercise 1
+def string_shuffle(s)
+  s.split('').shuffle.join
+end
+
+#Test it works
+puts string_shuffle("foobar")
+
+#Exercise 2
+class String
+  def shuffle
+    self.split('').shuffle.join
+  end
+end
+
+#Test it works
+puts "foobar".shuffle 
+
+#Exercise 3
+person1 = { first: "Homer", last: "Simpson" }
+person2 = { first: "Marge", last: "Simpson" }
+person3 = { first: "Bart", last: "Simpson" }
+params  = { father: person1, mother: person2, child: person3 } 
+
+#Test it works
+puts params[:father][:first] 
